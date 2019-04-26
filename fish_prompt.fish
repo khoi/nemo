678a09446a30b06d968d98aa1823157c5f93629b
khoi <khoiracle@gmail.com>
2019-04-27 00:04:24 +0700

    more informative git prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,18 +1,26 @@
 # Git prompt
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_hide_untrackedfiles 1
+set __fish_git_prompt_showupstream 'informative'
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_stagedstate green
-set __fish_git_prompt_color_upstream cyan
+
+# Git color
+set __fish_git_prompt_color_dirtystate blue
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_cleanstate green
 
 # Git Characters
-set __fish_git_prompt_char_dirtystate '✗'
-set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stagedstate "●"
+set __fish_git_prompt_char_dirtystate "✚"
+set __fish_git_prompt_char_untrackedfiles "…"
+set __fish_git_prompt_char_conflictedstate "✖"
 set __fish_git_prompt_char_upstream_prefix ' '
 set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 set __fish_git_prompt_char_upstream_diverged '↑↓'
+set __fish_git_prompt_char_cleanstate "✔"
 
 function fish_prompt
   set -l last_status $status
